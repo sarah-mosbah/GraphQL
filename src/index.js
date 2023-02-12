69eaa2f02dc8999ea3168bbf43e81e1c0cd89194
sarah-mosbah <sarah@checkmehealth.com>
2023-02-12 12:23:14 +0200

    Start Mutations

--- src/index.js
+++ src/index.js
@@ -1,47 +1,84 @@
 import { createServer } from 'node:http';
 import { createYoga, createSchema } from 'graphql-yoga';
-
+import {uuid} from 'uuidv4';
+const comments = [{
+    id: 1,
+    text: 'Hello This is My Comment',
+    authorId: '2',
+    postId: '10'
+},{
+    id: 2,
+    text: 'Hello This is My Comment 2',
+    authorId: '3',
+    postId: '10'
+},
+{
+    id: 3,
+    text: 'Hello This is My Comment 3',
+    authorId: '1',
+    postId: '11'
+},
+{
+    id: 4,
+    text: 'Hello This is My Comment 4',
+    authorId: '2',
+    postId: '12'
+}]
 const users = [{
     id: '1',
     name: 'Andrew',
     email: 'andrew@example.com',
-    age: 27
+    age: 27,
+    comments: [3]
 }, {
     id: '2',
     name: 'Sarah',
-    email: 'sarah@example.com'
+    email: 'sarah@example.com',
+    comments: [1, 4]
 }, {
     id: '3',
     name: 'Mike',
-    email: 'mike@example.com'
+    email: 'mike@example.com',
+    comments: [2]
 }]
-// Type Definitions (schema)
+
 const posts = [{
     id: '10',
     title: 'GraphQL 101',
     body: 'This is how to use GraphQL...',
     published: true,
-    author: '1'
+    author: '1',
+    comments: [1, 2]
 }, {
     id: '11',
     title: 'GraphQL 201',
     body: 'This is an advanced GraphQL post...',
     published: false,
-    author: '1'
+    author: '1',
+    comments: [3]
 }, {
     id: '12',
     title: 'Programming Music',
     body: '',
     published: false,
-    author: '2'
+    author: '2',
+    comments: [4]
 }]
-
+// Type Definitions (schema)
 const typeDefs = `
     type Query {
        users: [User!]!
        me: User!
        post: Post!
        posts(query: String): [Post!]!
+       comments: [Comment]
+    }
+    type Mutation {
+        createUser(name: String!, email: String!, age: Int): User!
+        createPost(title: String!
+            body: String!
+            published: Boolean!
+            author: User!): Post!
     }
     type User {
         id: ID!
@@ -49,6 +86,7 @@ const typeDefs = `
         email: String!
         age: Int
         posts: [Post!]!
+        comments: [Comment!]
     }
 
     type Post {
@@ -57,6 +95,14 @@ const typeDefs = `
         body: String!
         published: Boolean!
         author: User!
+        comments: [Comment!]
+    }
+
+    type Comment {
+        id: ID!
+        text: String!
+        author: User!
+        post: Post!
     }
 `;
 
@@ -68,7 +114,6 @@ const resolvers = {
                 id: 'qjdjdjdj',
                 title: 'my title',
                 email: "Helloojm@x.com",
-
             }
         },
         post: () => {
@@ -94,17 +139,48 @@ const resolvers = {
                 return user.name.toLowerCase().includes(args.query.toLowerCase())
             })
         },
+        comments(parent, args, ctx, info) {
+            return comments;
+        }
+    },
+    Mutation: {
+        createUser(parent, args, ctx, info) {
+           const emailTaken = users.some((user) => user.email === args.email);
+           if(emailTaken) throw new Error('Email is Taken');
+           const id  =  uuid();
+           const addedUser  =  {
+            email: args.email,
+            name: args.name,
+            age: args.age,
+            id
+           };
+           users.push(addedUser);
+           return addedUser;
+        }
     },
     Post: {
         author(parent, args)  {
             return users.find((user) => user.id === parent.author)
+        },
+        comments(parent, args) {
+            return comments.filter((comment) => comment.postId === parent.id)
         }
     },
     User: {
         posts(parent, args)  {
-            console.log(parent)
             return posts.filter((post) => post.author === parent.id)
+        },
+        comments(parent, args) {
+            return comments.filter((comment) => comment.authorId === parent.id)
         }
+    },
+    Comment: {
+        author(parent, args) {
+            return users.find((user) => user.id === parent.authorId)
+        },
+        post(parent, args)  {
+            return posts.find((post) => post.id === parent.postId)
+        },
     }
 }
 const schema = createSchema({
@@ -112,7 +188,7 @@ const schema = createSchema({
 });
 
 
-const yoga = createYoga({ schema });
+const yoga = createYoga({ schema,  graphqlEndpoint: '/', });
  
 // Pass it into a server to hook into request handlers.
 const server = createServer(yoga);
